cdd50190debe5ac653ef56bfab1d1c2529e5c063
michaelj-github <mike@michaeljmurphy.com>
2022-06-20 12:20:23 -0400

    update part_one app.js

--- promises/part_one/app.js
+++ promises/part_one/app.js
@@ -1,21 +1,21 @@
 let URL = "http://numbersapi.com/23?json";
 
-console.log("#1 - console.log(response)");          
+console.log("#1 - console.log(response)");
 
 $.getJSON(`${URL}`)
-    .then(response => {
-    console.log(response);          
-});    
+    .then(r => {
+    console.log(r.number, r.text);
+});
 
 URL = "http://numbersapi.com/5,17,23?json";
 
 $.getJSON(`${URL}`)
-    .then(response => {
+    .then(r => {
     $("body").append(`<h1>#2 - Facts about the numbers 5, 17, and 23</h1>`);
-    $("body").append(`<p>${response[5]}</p>`);
-    $("body").append(`<p>${response[17]}</p>`);
-    $("body").append(`<p>${response[23]}</p>`);
-    });    
+    $("body").append(`<p>${r[5]}</p>`);
+    $("body").append(`<p>${r[17]}</p>`);
+    $("body").append(`<p>${r[23]}</p>`);
+    });
 
 URL = "http://numbersapi.com/23?json";
 let responseArray = []
@@ -24,5 +24,3 @@ for (let i = 1; i <= 4; i++) {
     responseArray.push($.getJSON(`${URL}`))
 }
 Promise.all(responseArray).then(rArray => ( $("body").append(`<h2>#3 - 4 facts about the number 23</h2>`), rArray.forEach(r => $("body").append(`<p>${r.text}</p>`))) )
-
-    
